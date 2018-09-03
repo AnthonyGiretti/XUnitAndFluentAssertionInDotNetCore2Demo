272e2e8d9aa889766fcd57128c0fb45de813b2ba
Anthony Giretti <anthony.giretti@gmail.com>
2018-09-03 17:34:25 -0400

    Add another unit test

--- UnitTests/CustomExceptionMiddlewareTests.cs
+++ UnitTests/CustomExceptionMiddlewareTests.cs
@@ -1,6 +1,7 @@
 ﻿using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
+using System;
 using System.IO;
 using System.Net;
 using System.Threading.Tasks;
@@ -39,6 +40,37 @@ namespace UnitTests
             context.Response.StatusCode
                 .Should()
                 .Be((int)HttpStatusCode.NotFound);
+
+        }
+
+        [Fact]
+        public async Task WhenAnUnExpectedExceptionIsRaised_CustomExceptionMiddlewareShouldHandleItToCustomErrorResponseAndInternalServerErrorHttpStatus()
+        {
+            // Arrange
+            var middleware = new CustomExceptionMiddleware(next: (innerHttpContext) =>
+            {
+                throw new Exception("Test");
+            });
+
+            var context = new DefaultHttpContext();
+            context.Response.Body = new MemoryStream();
+
+            //Act
+            await middleware.Invoke(context);
+
+            context.Response.Body.Seek(0, SeekOrigin.Begin);
+            var reader = new StreamReader(context.Response.Body);
+            var streamText = reader.ReadToEnd();
+            var objResponse = JsonConvert.DeserializeObject<CustomErrorResponse>(streamText);
+
+            //Assert
+            objResponse
+            .Should()
+            .BeEquivalentTo(new CustomErrorResponse { Message = "Unexpected error", Description = "Unexpected error" });
+
+            context.Response.StatusCode
+                .Should()
+                .Be((int)HttpStatusCode.InternalServerError);
 
         }
     }
