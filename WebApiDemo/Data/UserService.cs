0ddae734b21bd891aaf95eb20921b7f75a82d990
Anthony Giretti <anthony.giretti@gmail.com>
2018-09-05 20:34:45 -0400

    create a service with httpclient to test

---
+++ WebApiDemo/Data/UserService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebApiDemo.Data
+{
+    public class UserService
+    {
+        private readonly IHttpClientFactory _httpFactory;
+
+        public UserService(IHttpClientFactory httpFactory)
+        {
+            _httpFactory = httpFactory;
+        }
+
+        public async Task<List<Users>> GetUsers(string url)
+        {
+            using (HttpClient httpclient = _httpFactory.CreateClient())
+            using (HttpResponseMessage response = await httpclient.GetAsync(url))
+            {
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    List<Users> users = await response.Content.ReadAsAsync<List<Users>>();
+                    return users;
+                }
+                return null;
+                
+            }
+        }
+    }
+}
---
+++ WebApiDemo/Data/Users.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiDemo.Data
+{
+    public class Users
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
