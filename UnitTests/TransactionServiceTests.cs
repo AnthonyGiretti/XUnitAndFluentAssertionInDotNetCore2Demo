5fe143877b46180f9118d9891c153e0615350b5b
Anthony Giretti <anthony.giretti@gmail.com>
2019-01-02 00:19:11 -0500

    creating unit test with Datatable example

--- UnitTests/TransactionServiceTests.cs
+++ UnitTests/TransactionServiceTests.cs
@@ -5,7 +5,9 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Text;
+using WebApiDemo.BLL;
 using WebApiDemo.DAL;
+using WebApiDemo.Data;
 using Xunit;
 
 namespace UnitTests
@@ -15,8 +17,47 @@ namespace UnitTests
         [Fact]
         public void WhenGetTransactionsRepositoryReturnAFilledDataTable_GetTransactionsServiceReturnsAListOfTransactions()
         {
-            
+            // Arrange
+            ITransactionRepository transactionRepositoryMock = Substitute.For<ITransactionRepository>();
 
+            //DataTable table = new DataTable("MyTable");
+            //DataColumn idColumn = new DataColumn("id", typeof(int));
+            //DataColumn amountColumn = new DataColumn("amount", typeof(decimal));
+            //DataColumn dateColumn = new DataColumn("date", typeof(DateTime));
+
+            //table.Columns.Add(idColumn);
+            //table.Columns.Add(amountColumn);
+            //table.Columns.Add(dateColumn);
+
+            //DataRow newRow = table.NewRow();
+            //newRow["id"] = 1;
+            //newRow["amount"] = 10.3m;
+            //newRow["date"] = new DateTime(2018, 10, 20);
+            //table.Rows.Add(newRow);
+
+            //newRow = table.NewRow();
+            //newRow["id"] = 2;
+            //newRow["amount"] = 42.1m;
+            //newRow["date"] = new DateTime(2018, 04, 12);
+            //table.Rows.Add(newRow);
+
+            //newRow = table.NewRow();
+            //newRow["id"] = 2;
+            //newRow["amount"] = 5.6;
+            //newRow["date"] = new DateTime(2018, 07, 2);
+            //table.Rows.Add(newRow);
+
+            var datatable = EmbeddedJsonFileHelper.GetContent<DataTable>(@"Files\datatable.transactions");
+            var expectedResult = EmbeddedJsonFileHelper.GetContent<List<Transaction>>(@"Files\list.transactions").ToExpectedObject();
+
+            transactionRepositoryMock.GetTransactions().Returns(x => datatable);
+
+            // Act
+            var service = new TransactionService(transactionRepositoryMock);
+            var result = service.GetTransactions();
+
+            // Assert
+            expectedResult.ShouldEqual(result);
         }
     }
 }
